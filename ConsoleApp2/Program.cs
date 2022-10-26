a6f0c3b154b42cd8fa6708cfe9a73952f3b9c3c7
Henrique Greghi <henriquevsg@gmail.com>
2022-10-26 15:15:47 -0300

    Operador Contains

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "1234567890";
+
+            if (a.Contains ("345"))
+            {
+                Console.WriteLine("deu certo");
+            }
+            else
+            {
+                Console.WriteLine("deu errado");
+            }
+        }
+    }
+}
